656130821b1d3395d7775ee2e71f61c5eb9fc45c
Eduarda Timóteo <eduarda.silva7739@gmail.com>
2025-05-24 17:44:55 -0300

    Add files via upload

---
+++ desafio11.py
@@ -0,0 +1,7 @@
+altura = float(input('Largura da parede: '))
+largura = float(input('Altura da parede: '))
+
+área = altura * largura
+tinta = área / 2
+
+print (f' Sua parede possui a dimesão de {altura}x{largura} e sua área é de {área}m2 \n Para pintar essa parede, você precisará de {tinta}L de tinta ')
