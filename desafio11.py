ee571070ca426553680a8d1925cc4b2d4714e914
Eduarda Timóteo <eduarda.silva7739@gmail.com>
2025-06-18 18:24:34 -0300

    Add files via upload

--- desafio11.py
+++ desafio11.py
@@ -1,3 +1,8 @@
+nome = input('Qual o seu nome ? ') 
+print('================================================================')
+
+print(f'{nome}, antes de realizar a pintura, segue abaixo algumas orientações! \n 1°:. Isolar o ambiente protegendo o piso, portas e paredes. Para realizar está etapa é preciso de uma lona preta e uma fita crepe \n 2°:. Terá que realizar pre-paração da parede com uma licha de 80(licha de ferro). Situação utilizada caso a parede esteja no reboco \n 3°:.Para passar a massa corrida nas paredes, ira precisar de uma empenadeira e uma espatula. Ao utilizar a empenadeira deixe inclina um pouco para a parede \n Observação:. Após esperar a massa secar, terá que "consertar" passando a licha 80, para prossegeuir com a próxima etapa \n 4°:. O aconselhado é que utilize uma tinta o qual tenha um custo beneficio. Materiais adicional é um rolo e pincel de tinta ')
+
 altura = float(input('Largura da parede: '))
 largura = float(input('Altura da parede: '))
 
